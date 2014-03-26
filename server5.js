d446a87797c935e89ac36335a271dc87c60fcae7
thangskyline <thangskyline@yahoo.com>
2014-03-26 22:26:36 +0700

    sdf

--- server5.js
+++ server5.js
@@ -1,1 +1,14 @@
-var i = 7;
+// Load the http module to create an http server.
+var http = require('http');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
+});
+
+// Listen on port 82, IP defaults to 127.0.0.1
+server.listen(82);
+
+// Put a friendly message on the terminal
+console.log("Server running at port 82");
